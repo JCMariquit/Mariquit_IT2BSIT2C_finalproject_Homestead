da4bc5a0f7313c773c01174fc80d5782d5a81088
JCharles <junecharlesmariquit553@gmail.com>
2023-05-26 22:10:18 +0800

    initalizing body header and footer

---
+++ design/home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body>
+<!-- home section starts  -->
+<p class="br1"></p>
+<section class="home" id="home">
+
+    <div class="image" data-aos="fade-down">
+        <img src="images/Logo/logo.png" alt="">
+        <p class="br1">
+    </div>
+
+    <div class="content" data-aos="fade-up">
+        <h3>Music Gallery & Books</h3>
+        <p >Art is knowing which ones to keep. Music is the art which is most nigh to tears and memories. Music can change the world because it can change people. Music is God's gift to man, the only art of Heaven given to earth, the only art of earth we take to Heaven.</p>
+        <p class="br1">
+        
+    </div>
+</section>
+
+<!-- home section ends -->
+</body>
+</html>
